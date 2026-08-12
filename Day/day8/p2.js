4853289802c23d071cf217a369241069d9562cd3
naikvinod748-arch <naikvinod748@gmail.com>
2026-08-12 12:27:00 +0530

    var,let and const

---
+++ Day/day8/p2.js
@@ -0,0 +1,14 @@
+//js variable
+//we can craete variable using var | let | const
+//var is not a block scoped
+//let is  a block scope i.e, cannot access outside the scope
+//const is a block scope and it cannot be access outside the scope and we can create const with same name inside the scope
+var x="10";
+var x="20";
+var y="30"
+console.log(x)
+z=x+y;
+console.log(z)
+//let cannot be used create with same name
+let name="vinod";
+console.log(name)
